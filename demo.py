f82f7a7128c41bb7dc88537ca3a9e214495779fb
prachibagkar <72984623+prachibagkar@users.noreply.github.com>
2021-10-29 22:55:50 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,10 @@
+print('My Details')
+print('------------------------------')
+print('''
+Name: Prachi Bagkar
+Class/Div: TE6
+Roll No: 04
+Department: IT
+Batch: A
+''')
+print('------------------------------')
